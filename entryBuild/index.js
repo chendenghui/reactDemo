5a8de5380ed9b3506eb68d45c90af850a1fc625c
denghui.chen <denghui.chen@wormpex.com>
2018-03-22 11:12:02 +0800

    成功

--- entryBuild/index.js
+++ entryBuild/index.js
@@ -5,6 +5,6 @@ import React from 'react';
 
 import ReactDOM from 'react-dom';
 
-import Index from '../app/component/index/index.jsx';
+import Index from '../app/component/index/Index.jsx';
 
 ReactDOM.render(<Index/>,document.getElementById('app'));
